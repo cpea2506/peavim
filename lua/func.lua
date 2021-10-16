2a5ae4e09f7d6474b3b741ef7a77eb136127208c
WeakCookie <cpea2506@Viens-Mac.local>
2021-10-16 16:58:06 +0700

    move some functions to where it belongs

--- lua/func.lua
+++ lua/func.lua
@@ -1,30 +1,5 @@
 local M = {}
 
-M.align_center = function(lines, alignment)
-	local output = {}
-	local max_len = 0
-
-	for _, line in ipairs(lines) do
-		local line_len = #line
-		if line_len > max_len then
-			max_len = line_len
-		end
-	end
-
-	for _, line in ipairs(lines) do
-		local padding = string.rep(" ", (math.max(0, max_len) - line:len()) * alignment)
-		table.insert(output, padding .. line)
-	end
-
-	return output
-end
-
-M.has_words_before = function()
-	local cursor = vim.api.nvim_win_get_cursor(0)
-	local line, col = cursor[1], cursor[2]
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local function set_options(scope, opts)
 	for opt, value in pairs(opts) do
 		scope[opt] = value
@@ -44,7 +19,7 @@ end
 M.call_plugins = function(plugs)
 	local plug_dir = "plugins.custom"
 	for _, plug in pairs(plugs) do
-		require(plug_dir .. "." .. plug)
+		require(plug_dir .. "." .. plug).setup()
 	end
 end
 
