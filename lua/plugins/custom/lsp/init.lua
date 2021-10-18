128652ff9ae6d36466255cecf6140a9eaffe0c69
cpea2506 <cpea2506@Viens-Mac.local>
2021-10-18 20:37:10 +0700

    config to avoid conflict with null-ls

--- lua/plugins/custom/lsp/init.lua
+++ lua/plugins/custom/lsp/init.lua
@@ -1,24 +1,47 @@
 local M = {}
 
-M.setup = function()
-	local lsp_installer = require("nvim-lsp-installer")
+-- lsputils for better code actions and more
+local set_handler = function()
+	vim.lsp.handlers["textDocument/codeAction"] = require("lsputil.codeAction").code_action_handler
+	vim.lsp.handlers["textDocument/references"] = require("lsputil.locations").references_handler
+	vim.lsp.handlers["textDocument/definition"] = require("lsputil.locations").definition_handler
+	vim.lsp.handlers["textDocument/declaration"] = require("lsputil.locations").declaration_handler
+	vim.lsp.handlers["textDocument/typeDefinition"] = require("lsputil.locations").typeDefinition_handler
+	vim.lsp.handlers["textDocument/implementation"] = require("lsputil.locations").implementation_handler
+end
 
-	local on_attach = function()
-		require("key_mapping").set_keymap({
-			normal = {
-				["<C-a>"] = ":lua vim.lsp.buf.code_action()<CR>",
-			},
-		})
-	end
-
+local capabilities = function()
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	capabilities.textDocument.completion.completionItem.snippetSupport = true
+	capabilities.textDocument.completion.completionItem.resolveSupport = {
+		properties = {
+			"documentation",
+			"detail",
+			"additionalTextEdits",
+		},
+	}
 	capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
+	return capabilities
+end
+
+local on_attach = function(client, _)
+	for _, server in pairs(pea.builtin.lsp.disable_fmt) do
+		if client.name == server then
+			client.resolved_capabilities.document_formatting = false
+			client.resolved_capabilities.document_range_formatting = false
+		end
+	end
+end
+
+M.setup = function()
+	set_handler()
+
+	local lsp_installer = require("nvim-lsp-installer")
 	lsp_installer.on_server_ready(function(server)
 		local opts = {
 			on_attach = on_attach,
-			capabilities = capabilities,
+			capabilities = capabilities(),
 		}
 
 		server:setup(opts)
