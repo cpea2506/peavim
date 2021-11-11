ca9c22cb7bf084363d275e322fe30666256c1612
cpea2506 <vienpro2506@gmail.com>
2021-11-11 20:17:37 +0700

    add indent line

---
+++ lua/plugins/custom/indent_blankline.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+local custom = {
+	indent_blankline_color = {
+		{ "ColorScheme", "*", "hi IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine" },
+		{ "ColorScheme", "*", "hi IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine" },
+		{ "ColorScheme", "*", "hi IndentBlanklineIndent3 guifg=#98C379 gui=nocombine" },
+		{ "ColorScheme", "*", "hi IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine" },
+		{ "ColorScheme", "*", "hi IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine" },
+		{ "ColorScheme", "*", "hi IndentBlanklineIndent6 guifg=#C678DD gui=nocombine" },
+	},
+}
+
+M.setup = function()
+	autocmd.define_augroups({ indent_blankline_color = custom.indent_blankline_color })
+
+	require("indent_blankline").setup({
+		use_treesitter = true,
+		strict_tabs = true,
+		buftype_exclude = { "terminal" },
+		filetype_exclude = { "help", "lsp", "packer", "toggleterm", "NvimTree", "dashboard", "md" },
+		space_char_blankline = " ",
+		char_highlight_list = {
+			"IndentBlanklineIndent1",
+			"IndentBlanklineIndent2",
+			"IndentBlanklineIndent3",
+			"IndentBlanklineIndent4",
+			"IndentBlanklineIndent5",
+			"IndentBlanklineIndent6",
+		},
+	})
+end
+
+return M
