7c6cac5cba535ecb9192242c60a3faff8902d480
cpea2506 <vienpro2506@gmail.com>
2021-11-17 14:23:22 +0700

    remove deprecated use_lspsaga, increase max_width and reduce time_interval

--- lua/pea/plugins/lsp/lsp_signatures.lua
+++ lua/pea/plugins/lsp/lsp_signatures.lua
@@ -10,17 +10,16 @@ local config = {
 	hint_enable = true,
 	hint_prefix = "🐼 ",
 	hint_scheme = "String",
-	use_lspsaga = true,
 	hi_parameter = "LspSignatureActiveParameter",
 	max_height = 12,
-	max_width = 120,
+	max_width = 150,
 	transparency = 10,
 	always_trigger = true,
 	auto_close_after = 20,
 	extra_trigger_chars = { "(", "," },
 	zindex = 200,
 	padding = "",
-	timer_interval = 200,
+	timer_interval = 100,
 }
 
 M.setup = function()
