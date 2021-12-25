0b90376c745d6053169121ef5c27ff550182b047
CPea <42694704+cpea2506@users.noreply.github.com>
2021-12-26 01:10:45 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 require("config.packer")
 
-require("pea.utils.func").call_setup({
+--[[require("pea.utils.func").call_setup({
 	"config.default",
 	"utils.autocmd",
 	"utils.key_mapping",
 	"plugins",
 }, "pea")
 
-require("pea.config")
+require("pea.config")]]
 
 -- default theme
 vim.cmd([[colorscheme one_monokai]])
